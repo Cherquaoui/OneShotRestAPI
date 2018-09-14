7acb24c7aefdc04ef42f32b05996087b3a197952
Cherquaoui <cherq.habib@gmail.com>
2018-09-14 18:47:13 +0100

    test heroku

--- src/main/java/sbi/oneshot/repositories/composition/OneShotRepository.java
+++ src/main/java/sbi/oneshot/repositories/composition/OneShotRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import sbi.oneshot.entities.composition.OneShot;
 
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface OneShotRepository extends PagingAndSortingRepository<OneShot,String> {
 
 
